f986c98b9402fc3818c319aff9009783a224d05b
Ankit <ankitgautam8898@gmail.com>
2022-07-11 18:12:59 +0530

    incomplete pagination

--- src/controller/contract.js
+++ src/controller/contract.js
@@ -4,6 +4,7 @@ import contractSchema from '../schema/contractSchema.js';
 
 const createContract = async (req, res) => {
 	try {
+		console.log('create Contract');
 		const validation = await contractSchema.validateAsync(req.body);
 
 		console.log(validation);
@@ -25,8 +26,9 @@ const createContract = async (req, res) => {
 					typeOfHours: validation.typeOfHours,
 					totalEntitlement: validation.totalEntitlement,
 					projectManager: validation.projectManager,
-					remarks: validation.remarks,
+					remarks: validation.remarks ?? ' ',
 					ownerId: validation.ownerId,
+					state: validation.state,
 				});
 				console.log(newContract);
 				const result = await newContract.save();
@@ -40,21 +42,149 @@ const createContract = async (req, res) => {
 			res.status(402).send(error);
 		}
 	} catch (error) {
+		console.log('error', error);
 		if (error.isJoi) {
-			res.status(401).send({
+			res.status(400).send({
 				ok: false,
 				error: error.details[0].message,
 			});
 		} else {
-			res.status(401).send({ ok: false, error: error });
+			res.status(400).send({ ok: false, error: error });
 		}
 	}
 };
-
+const editContract = async (req, res) => {
+	try {
+		const result = await contractSchema.validateAsync(req.body);
+		const contract = await ContractModel.findOne({ id: result.id });
+		if (contract) {
+			res.status(400).json({ ok: true, message: contract });
+		} else {
+			res.status(400).json({ ok: false, error: 'Contract not found' });
+		}
+	} catch (error) {
+		if (error.isJoi) {
+			res.status(400).send({
+				ok: false,
+				error: error.details[0].message,
+			});
+		} else {
+			res.status(400).send({ ok: false, error: error });
+		}
+	}
+};
 const getContract = async (req, res) => {
 	const result = await ContractModel.find();
 	res.send({ ok: true, contracts: result });
 };
 
+const paginated = async (req, res) => {
+	const {
+		page = 1,
+		limit = 10,
+		filterMode = '009',
+		filterColumn = '',
+	} = req.query;
+	const number = '009';
+	try {
+		console.log('filterMode', typeof filterMode, filterMode);
+		// execute query with page and limit values
+		if (filterColumn === 'id') {
+			console.log('filter for id');
+
+			const contract = await ContractModel.find({
+				[filterColumn]: { $gte: filterMode },
+			})
+				.limit(limit * 1)
+				.skip((page - 1) * limit)
+				.exec();
+			const count = await ContractModel.find({
+				[filterColumn]: { $gte: filterMode },
+			}).count();
+
+			// return response with posts, total pages, and current page
+			res.json({
+				ok: true,
+				contract: contract,
+				totalPages: Math.ceil(count / limit),
+				currentPage: page,
+				totalRow: count,
+			});
+		} else if (
+			filterColumn === 'contractName' ||
+			filterColumn === 'company' ||
+			filterColumn === 'servicePackage' ||
+			filterColumn === 'typeOfHours' ||
+			filterColumn === 'projectManager' ||
+			filterColumn === 'remarks'
+		) {
+			console.log('filter for string');
+
+			const newRegex = new RegExp(`${filterMode}`);
+			const contract = await ContractModel.find({
+				[filterColumn]: { $regex: newRegex },
+			})
+				.limit(limit * 1)
+				.skip((page - 1) * limit)
+				.exec();
+			const count = await ContractModel.find({
+				[filterColumn]: { $regex: newRegex },
+			}).count();
+
+			// return response with posts, total pages, and current page
+			res.json({
+				ok: true,
+				contract: contract,
+				totalPages: Math.ceil(count / limit),
+				currentPage: page,
+				totalRow: count,
+			});
+		} else if (filterColumn === 'assets') {
+			console.log('filterColumn', filterColumn.toLowerCase());
+			const arrayofData = filterMode.split(',');
+			console.log('arrayofData', arrayofData);
+
+			const contract = await ContractModel.find({
+				[filterColumn.toLowerCase()]: { $in: arrayofData },
+			})
+				.limit(limit * 1)
+				.skip((page - 1) * limit)
+				.exec();
+			const count = await ContractModel.find({
+				[filterColumn]: { $in: arrayofData },
+			}).count();
+
+			// return response with posts, total pages, and current page
+			res.json({
+				ok: true,
+				contract: contract,
+				totalPages: Math.ceil(count / limit),
+				currentPage: page,
+				totalRow: count,
+			});
+		} else {
+			console.log('All data');
+			const contract = await ContractModel.find({})
+				.limit(limit * 1)
+				.skip((page - 1) * limit)
+				.exec();
+			console.log('contract', contract);
+			const count = await ContractModel.countDocuments();
+
+			res.json({
+				ok: true,
+				contract: contract,
+				totalPages: Math.ceil(count / limit),
+				currentPage: page,
+				totalRow: count,
+			});
+		}
+
+		// get total documents in the Posts collection
+	} catch (err) {
+		console.log('Error', err);
+		res.status(400).send({ ok: false, error: err });
+	}
+};
 export default createContract;
-export { getContract };
+export { getContract, editContract, paginated };
--- src/route/contract/contractRoute.js
+++ src/route/contract/contractRoute.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import createContract, { getContract } from '../../controller/contract.js';
+import createContract, {
+	editContract,
+	getContract,
+	paginated,
+} from '../../controller/contract.js';
 const Router = express.Router();
 
-const contractRoute = Router.post('/api/v1/createContract', createContract).get(
-	'/api/v1/getContract',
-	getContract
-);
+const contractRoute = Router.post('/api/v1/createContract', createContract)
+	.get('/api/v1/getContract', getContract)
+	.get('/api/v1/paginated', paginated)
+	.put('/api/v1/editContract', editContract);
 
 export default contractRoute;
